17ea385ef451c7ec94db8e97b2fc0371b9fb583f
dev-vitorAugusto <vitor.souza0295@gmail.com>
2025-04-19 13:02:38 -0300

    adicionando tela de compras

--- scripts.js
+++ scripts.js
@@ -64,3 +64,95 @@ fetch(url)
     document.getElementById('produtos').textContent = 'Erro ao carregar os produtos.';
   });
 //   FIM
+
+// SISTEMA DE ENVIO DE FORMULÁRIO E PRODUTOS - CARRINHO
+const carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+const lista = document.getElementById('lista');
+const cliente = document.getElementById('clienteNome').value; 
+let total = 0;
+let mensagem = `Olá! me chamo ${cliente} Gostaria de fazer um pedido:\n`;
+
+
+
+const tb = document.getElementById('tabela');
+  
+  const div = document.createElement('table');
+  div.className = 'table table-hover table-sm table-borderless mt-4 ';
+  div.innerHTML = `
+      <table class="">
+            <thead class="tab-cabecalho">
+              <tr>
+                <th scope="col">produtos</th>
+                <th scope="col">Preço</th>
+                <th scope="col">Subtotal</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+  `;
+  const tbody = div.querySelector('tbody');
+
+
+  carrinho.forEach((item, i) => {
+    const subtotal = item.preco * item.quantidade;
+    total += subtotal;
+    mensagem += `• ${item.quantidade}x ${item.nome} - R$ ${subtotal.toFixed(2)}\n`;
+
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td>${item.quantidade}x ${item.nome}</td>
+      <td>R$${item.preco}</td>
+      <td>R$${subtotal.toFixed(2)}</td>
+    `;
+    tbody.appendChild(tr)
+  });
+  tb.appendChild(div);
+
+const texto = document.createElement('h5');
+texto.textContent = `\nR$ ${total.toFixed(2)}`;
+lista.appendChild(texto)
+
+mensagem += `\nTotal: R$ ${total.toFixed(2)}`;
+
+function enviarPedido() {
+  const nome = document.getElementById('clienteNome').value;
+  const endereco = document.getElementById('clienteEndereco').value;
+  const pagamento = document.getElementById('pagamento').value;
+  const obs = document.getElementById('observacoes').value;
+  const entregaDiv = document.getElementById("entrega");
+  const checkboxes = entregaDiv.querySelectorAll("input[type='radio']");
+  let selecionados = [];
+
+  checkboxes.forEach((checkbox) => {
+        if(checkbox.checked) {
+            selecionados.push(checkbox.value);
+        }
+      }
+      );
+
+  let mensagem = `Olá! Me chamo ${nome} e gostaria de fazer um pedido:\n\n`;
+
+  carrinho.forEach(item => {
+    const subtotal = item.preco * item.quantidade;
+    mensagem += `• ${item.quantidade}x ${item.nome} - R$ ${subtotal.toFixed(2)}\n`;
+  });
+
+  mensagem += `\n*Forma de pagamento:* ${pagamento}`;
+  mensagem += `\n*Entrega:* ${selecionados}`;
+  if (endereco) mensagem += `\n*Endereço:* ${endereco}`;
+  if (obs) mensagem += `\n*Observações:* ${obs}`;
+  mensagem += `\n*Total: R$* ${total.toFixed(2)}`;
+
+
+  const numero = "5511914060303"; // Substitua pelo número da doceira
+  const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+  window.open(url, "_blank");
+}
+
+function limparCarrinho(){
+  localStorage.removeItem('carrinho');
+  alert('Carrinho vazio!')
+  location.reload();
+}
